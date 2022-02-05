2c1a356542dce7cffe7df94de71779fe019d4c4b
hsedaqat1378 <hoseinsedaqat6@gmail.com>
2022-02-05 08:18:43 +0330

    rebuild the structore

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return ( 
+        <>
+        <header>
+            <h3>This is Header and we are beautiful it</h3>
+        </header>
+        </>
+     );
+}
+
+export default Header;
