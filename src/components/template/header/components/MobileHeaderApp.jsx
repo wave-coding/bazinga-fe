8355a10a95d5a705bff006317123aa80f7f7d7db
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-02-27 19:46:56 +0330

    add/responsive-header-for-mobile

--- src/components/template/header/components/MobileHeaderApp.jsx
+++ src/components/template/header/components/MobileHeaderApp.jsx
@@ -1,5 +1,6 @@
 import { HiBars3CenterLeft } from "react-icons/hi2";
 import { RiMapPinUserFill } from "react-icons/ri";
+import { MdSearch, MdOutlinePlace } from "react-icons/md";
 import { BsCart2 } from "react-icons/bs";
 import { useGlobalContext } from "@/context/useContext";
 import AmazonLogo from "@/assets/media/logo/Amazon-logo.png";
@@ -26,7 +27,7 @@ function MobileHeaderApp() {
             </li>
           </ul>
         </div>
-        <div className="info-menu">
+        <div className='info-menu'>
           <div>
             <h4>Hosein ,</h4>
             <RiMapPinUserFill size={20} />
@@ -38,6 +39,50 @@ function MobileHeaderApp() {
           </div>
         </div>
       </nav>
+      {/* search-bar */}
+      <section className='mobile-search'>
+        <div>
+          <input
+            type='text'
+            name='search'
+            id='search'
+          />
+        </div>
+        <div>
+          <button>
+            <MdSearch size={25}></MdSearch>
+          </button>
+        </div>
+      </section>
+      {/* Deals */}
+      <section className='mobile-deals'>
+        <ul>
+          <li>
+            <p>Deals</p>
+          </li>
+          <li>
+            <p>Amazon Basics</p>
+          </li>
+          <li>
+            <p>Buy Again</p>
+          </li>
+          <li>
+            <p>Video</p>
+          </li>
+          <li>
+            <p>Best Seller</p>
+          </li>
+        </ul>
+      </section>
+      {/* Deliver */}
+      <section className='mobile-deliver'>
+        <div>
+          <MdOutlinePlace size={15}/>
+        </div>
+        <div>
+          Deliver to Iran, Bandar-e-Abbas
+        </div>
+      </section>
     </>
   );
 }
