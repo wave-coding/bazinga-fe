0e036b76f9c36d60925fe6913c8a271f980b24f7
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-03-06 09:56:04 +0330

    put-product-to-landing

--- src/components/product-row/components/ProductRowApp.jsx
+++ src/components/product-row/components/ProductRowApp.jsx
@@ -14,80 +14,80 @@ function ProductRowApp() {
           </div>
           <div className='column-single-product'>
             <article>
-              <h4>Health & Personal Care</h4>
+              <h4>Kindle Unlimited</h4>
               <img
-                src='https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/GW_CONS_AUS_HPC_HPCEssentials_CatCard_Desktop1x._SY304_CB627424361_.jpg'
+                src='https://m.media-amazon.com/images/I/415Tfn6RqiL._SX370_.jpg'
                 alt=''
               />
             </article>
           </div>
           <div>
-            <h4>Gaming accessories</h4>
+            <h4>Your wellness favorites</h4>
             <div className='column'>
               <div className='column-1'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_1x._SY116_CB667159060_.jpg'
+                  src='https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2022/img/Consumables/Events/Deepi/BeautynWellnessGuide/AMZ-Winter23-BeautyWellnessGuide_QuadCard_Supplements-D-186x116_EG._SY116_CB613978413_.jpg'
                   alt=''
                 />
-                <p>headset</p>
+                <p>Supplements</p>
               </div>
               <div className='column-2'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_1x._SY116_CB667159063_.jpg'
+                  src='https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2022/img/Consumables/Events/Deepi/BeautynWellnessGuide/AMZ-Winter23-BeautyWellnessGuide_QuadCard_Fitness-D-186x116_EG._SY116_CB613978413_.jpg'
                   alt=''
                 />
-                <p>keyboards</p>
+                <p>Fitness</p>
               </div>
             </div>
             <div className='column'>
               <div className='column-1'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_1x._SY116_CB667159060_.jpg'
+                  src='https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2022/img/Consumables/Events/Deepi/BeautynWellnessGuide/AMZ-Winter23-BeautyWellnessGuide_QuadCard_Snacks-D-186x116_EG._SY116_CB613978413_.jpg'
                   alt=''
                 />
-                <p>headset</p>
+                <p>Snacks</p>
               </div>
               <div className='column-2'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_1x._SY116_CB667159063_.jpg'
+                  src='https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2023/img/Consumables/XCM_CUTTLE_1543938_2922214_186x116_1X_en_US._SY116_CB612443914_.jpg'
                   alt=''
                 />
-                <p>keyboards</p>
+                <p>Beverages</p>
               </div>
             </div>
           </div>
           <div>
-            <h4>Gaming accessories</h4>
+            <h4>Recommended for you from our brands</h4>
             <div className='column'>
               <div className='column-1'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_1x._SY116_CB667159060_.jpg'
+                  src='https://m.media-amazon.com/images/I/814XcLAe02L._AC_SY170_.jpg'
                   alt=''
                 />
-                <p>headset</p>
+                {/* <p>headset</p> */}
               </div>
               <div className='column-2'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_1x._SY116_CB667159063_.jpg'
+                  src='https://m.media-amazon.com/images/I/81nc9mU1XwL._AC_SY170_.jpg'
                   alt=''
                 />
-                <p>keyboards</p>
+                {/* <p>keyboards</p> */}
               </div>
             </div>
             <div className='column'>
               <div className='column-1'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_1x._SY116_CB667159060_.jpg'
+                  src='https://m.media-amazon.com/images/I/71lcImJBwrS._AC_SY170_.jpg'
                   alt=''
                 />
-                <p>headset</p>
+                {/* <p>headset</p> */}
               </div>
               <div className='column-2'>
                 <img
-                  src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_1x._SY116_CB667159063_.jpg'
+                  src='https://m.media-amazon.com/images/I/81BHlUPQDhL._AC_SY170_.jpg'
                   alt=''
                 />
-                <p>keyboards</p>
+                {/* <p>keyboards</p> */}
               </div>
             </div>
           </div>
