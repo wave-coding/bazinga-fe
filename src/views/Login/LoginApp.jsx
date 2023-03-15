bcc563f802a99e5700b42a69cc039b5e6f38e23a
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-03-15 22:34:50 +0330

    add/login-content

--- src/views/Login/LoginApp.jsx
+++ src/views/Login/LoginApp.jsx
@@ -1,7 +1,28 @@
+import AuthLogo from "@/assets/media/logo/auth-logo.png";
+
 function LoginApp() {
   return (
     <>
-      <h1>Fix and bring some app to views okay doky</h1>
+      <section className='login-container'>
+        <div className='login-logo'>
+          <img
+            src={AuthLogo}
+            alt='Auth_Logo'
+          />
+        </div>
+        <div className='login-content'>
+          <h3>Create account</h3>
+          <div className='login-form'>
+            <div className='login-form-content'>
+              <label htmlFor='Name'>Your name</label>
+              <input
+                type='text'
+                name='Name'
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
