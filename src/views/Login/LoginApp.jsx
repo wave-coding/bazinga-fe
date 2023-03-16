fa051cc90f81b0890a8b9dc947e581cef00f81e3
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-03-16 17:38:44 +0330

    add/finish-login

--- src/views/Login/LoginApp.jsx
+++ src/views/Login/LoginApp.jsx
@@ -20,6 +20,44 @@ function LoginApp() {
                 name='Name'
               />
             </div>
+            <div className='login-form-content'>
+              <label htmlFor='Email'>Email</label>
+              <input
+                type='email'
+                name='Email'
+              />
+            </div>
+            <div className='login-form-content'>
+              <label htmlFor='Password'>Password</label>
+              <input
+                type='password'
+                name='Password'
+                placeholder='At least 6 characters'
+              />
+            </div>
+            <div className='login-form-content'>
+              <label htmlFor='RePassword'>Re-enter password</label>
+              <input
+                type='password'
+                name='RePassword'
+                placeholder='password'
+              />
+            </div>
+            <div className='login-button'>
+              <button>
+                <p>Create Your Amazon account</p>
+              </button>
+            </div>
+            <div className='login-term'>
+              <small>
+                By creating an account, you agree to Amazon's Conditions of Use
+                and Privacy Notice.
+              </small>
+            </div>
+            <div className='switch-to-register'>
+              <small>Already have an account ? </small>
+              <small>Sign-In</small>
+            </div>
           </div>
         </div>
       </section>
