026512f1b1c9a1413b9255b7162b116bd06026ec
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-03-18 21:24:35 +0330

    add/finish-signup-styles

--- src/views/Signup/SignupApp.jsx
+++ src/views/Signup/SignupApp.jsx
@@ -1,7 +1,54 @@
+import AuthLogo from "@/assets/media/logo/auth-logo.png";
+
 function SignupApp() {
   return (
     <>
-      <h1>Sign Up</h1>
+      <section className='signup-container'>
+        <div className='signup-logo'>
+          <img
+            src={AuthLogo}
+            alt='Auth_Logo'
+          />
+        </div>
+        <div className='signup-content'>
+          <h3>Sign-In</h3>
+          <div className='signup-form'>
+            <div className='signup-form-content'>
+              <label htmlFor='Email'>Email</label>
+              <input
+                type='email'
+                name='Email'
+              />
+            </div>
+            <div className='signup-form-content'>
+              <label htmlFor='Password'>Password</label>
+              <input
+                type='password'
+                name='Password'
+              />
+            </div>
+            <div className='signup-button'>
+              <button>
+                <p>Sign In</p>
+              </button>
+            </div>
+            <div className='signup-term'>
+              <small>
+                By creating an account, you agree to Amazon's Conditions of Use
+                and Privacy Notice.
+              </small>
+            </div>
+            <div className='switch-to-login'>
+              <small>New to Amazon ? </small>
+            </div>
+            <div className='switch-to-login-button'>
+              <button>
+                <p>Create your Amazon Account</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
