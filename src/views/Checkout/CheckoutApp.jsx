159ca9d3b86bede9611e04e36b7bbd13d07ad9e4
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-03-22 03:15:49 +0430

    add/checkout-page

--- src/views/Checkout/CheckoutApp.jsx
+++ src/views/Checkout/CheckoutApp.jsx
@@ -1,7 +1,66 @@
+import MobileHeaderApp from "@/components/template/header/components/MobileHeaderApp";
+import Checkout_Sponsored from "@/assets/media/sponsored/Checkout_Img.jpg";
+import Header from "@/components/template/header/Header";
+import Footer from "@/components/template/footer/Footer";
+import Menu from "@/components/menu/Menu";
 function CheckoutApp() {
   return (
     <>
-      <h3>This is Checkout Page</h3>
+      <Header />
+      <Menu />
+      <MobileHeaderApp />
+      {/* Checkout Cotent */}
+      <section className='checkout'>
+        <div className='checkout-product'>
+          <div className='checkout-sponsered'>
+            <img
+              src={Checkout_Sponsored}
+              alt='Checkout_Sponsored'
+            />
+          </div>
+          <div className='checkout-shopping'>
+            <h2>Shopping Cart</h2>
+            <p>Price</p>
+            <div className='checkout-shopping-box'>
+              <article className='checkout-shopping-box-content'>
+                <div>
+                  <img
+                    src='https://images-fe.ssl-images-amazon.com/images/I/31jNXNVKclL._AC_AA160_.jpg'
+                    alt=''
+                    width={"20px"}
+                  />
+                  <p>text</p>
+                </div>
+                <div>$599.99</div>
+              </article>
+              <article className='checkout-shopping-box-content'>
+                <div>
+                  <img
+                    src='https://images-fe.ssl-images-amazon.com/images/I/31jNXNVKclL._AC_AA160_.jpg'
+                    alt=''
+                    width={"20px"}
+                  />
+                  <p>text</p>
+                </div>
+                <div>$599.99</div>
+              </article>
+            </div>
+            <p className='checkout-subtotal'>
+              Subtotal(2 items): <b>$628.08</b>
+            </p>
+          </div>
+        </div>
+        <div className='checkout-price'>
+          <div>
+            <p>
+              Subtotal(2 items): <b>$628.08</b>
+            </p>
+            <button>Procced to checkout</button>
+          </div>
+        </div>
+      </section>
+      {/* Checkout Cotent */}
+      <Footer />
     </>
   );
 }
