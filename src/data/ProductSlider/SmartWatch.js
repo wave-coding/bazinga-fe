f7fb65b3b30643ee82eceb9a0cf5bf080e374234
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-04-03 00:02:46 +0430

    completed/smartwatch-data

--- src/data/ProductSlider/SmartWatch.js
+++ src/data/ProductSlider/SmartWatch.js
@@ -44,31 +44,247 @@ export const smartWatchProduct = {
         },
         // https://www.amazon.com/Amazfit-Smartwatch-Military-Certified-Waterproof/dp/B08X9WS6VW/ref=sr_1_2_sspa?crid=3M4M3ZNLZM9GI&keywords=smart+watch&qid=1680029112&sprefix=smart+watc%2Caps%2C369&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFUVDNaNDFVUjQyS0wmZW5jcnlwdGVkSWQ9QTA3NjY5NjJNUlBKNk1ITlpVTVUmZW5jcnlwdGVkQWRJZD1BMDY2MTM2NDMzMEY0RlYyODEzMVImd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl
         {
+            id: `5ada865a-9ced-4669-8549-42e9414a6207`,
+            price: '141.45',
+            brand: 'Amazfit',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Round'
+                },
+                {
+                    title: 'Color',
+                    description: 'Black'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '1.3 Inches'
+                },
+                {
+                    title: 'Display Type',
+                    description: 'AMOLED'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71HvekeiFkL._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71HvekeiFkL._AC_SX679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71D2-OsX-hL._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71D2-OsX-hL._AC_SX679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71kLWFd5hoL._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71kLWFd5hoL._AC_SX679_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71HvekeiFkL._AC_SX679_.jpg',
             name: 'Amazfit T-Rex Pro Smart Watch for Men Rugged Outdoor GPS Fitness Watch, 15 Military Standard Certified, 100+ Sports Modes, 10 ATM Water-Resistant, 18 Day Battery Life, Blood Oxygen Monitor, Black'
         },
         // https://www.amazon.com/Samsung-Bluetooth-Advanced-Monitoring-Tracking/dp/B08HL5K8JB/ref=sr_1_5?crid=MODJTW6ASJI3&keywords=smart+watch&qid=1680029290&sprefix=smart+wat%2Caps%2C421&sr=8-5
         {
+            id: `ccfb932f-87c9-4515-82a2-f730cb1845db`,
+            price: '83.95',
+            brand: 'SAMSUNG',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Round'
+                },
+                {
+                    title: 'Color',
+                    description: 'Black'
+                },
+                {
+                    title: 'Style',
+                    description: 'Modern'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '34.5 Millimeters'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/818MAcfvjPL._AC_SY679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/818MAcfvjPL._AC_SY679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81fdlQqkeRL._AC_SY679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81fdlQqkeRL._AC_SY679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81uxZLm496L._AC_SY679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81uxZLm496L._AC_SY679_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/818MAcfvjPL._AC_SY679_.jpg',
             name: 'Samsung Galaxy Watch 3 (45mm, GPS, Bluetooth) Smart Watch with Advanced Health Monitoring, Fitness Tracking , and Long Lasting Battery - Mystic Black (US Version) (Renewed)'
         },
         // https://www.amazon.com/Touchscreen-Activity-Smartwatch-Signature-Charger/dp/B0B8L69BJW/ref=sr_1_17_sspa?crid=MODJTW6ASJI3&keywords=smart+watch&qid=1680029290&sprefix=smart+wat%2Caps%2C421&sr=8-17-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVUs0NkFVUEMwSzc0JmVuY3J5cHRlZElkPUEwODUyNjE1M1FaRzNSMDkzWExQRCZlbmNyeXB0ZWRBZElkPUEwNTIzMzkxMzBNQlhEUzMwNTZLMiZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=
         {
+            id: `a34dc604-38fc-495f-9f98-dbd12b34f4a6`,
+            price: '299.00',
+            brand: 'Certified Brands',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Round'
+                },
+                {
+                    title: 'Band Material Type',
+                    description: 'Leather'
+                },
+                {
+                    title: 'Style',
+                    description: 'Brown Leather Strap'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '1.28 Inches'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/6126A+NMC1L._AC_SX466_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/6126A+NMC1L._AC_SX466_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81b46lLzxvL._AC_SX466_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81b46lLzxvL._AC_SX466_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61gnpKqn8aL._AC_SX466_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61gnpKqn8aL._AC_SX466_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/6126A+NMC1L._AC_SX466_.jpg',
             name: `Fossil Gen 6 Men's 44mm Touchscreen GPS and Activity Smartwatch with Signature Series Charger Bundle`
         },
         // https://www.amazon.com/Apple-Cellular-Titanium-Precision-Extra-Long/dp/B0BDJK4G9C?ref_=ast_sto_dp
         {
+            id: `a3ced6d2-e881-470e-8f66-c81a9d04bc2a`,
+            price: '998.75',
+            brand: 'APPLE',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Square'
+                },
+                {
+                    title: 'Color',
+                    description: 'Titanium Case with Orange Alpine Loop'
+                },
+                {
+                    title: 'Style',
+                    description: 'GPS+Cellular'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '49 Millimeters'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/91z5KuonXrL.__AC_SY445_SX342_QL70_FMwebp_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/91z5KuonXrL.__AC_SY445_SX342_QL70_FMwebp_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81LNgb-7FnL._AC_SX522_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81LNgb-7FnL._AC_SX522_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/911Gc7IMlmL._AC_SX522_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/911Gc7IMlmL._AC_SX522_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/91z5KuonXrL.__AC_SY445_SX342_QL70_FMwebp_.jpg',
             name: `Apple Watch Ultra [GPS + Cellular 49mm] Smart Watch w/Rugged Titanium Case & Orange Alpine Loop Medium. Fitness Tracker, Precision GPS, Action Button, Extra-Long Battery Life, Brighter Retina Display`
         },
         // https://www.amazon.com/Apple-Cellular-Starlight-Aluminum-Always/dp/B0BDJ5SP5Q?ref_=ast_sto_dp
         {
+            id: `05997617-4f98-46cc-83a7-523539ee2485`,
+            price: '498.75',
+            brand: 'APPLE',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Square'
+                },
+                {
+                    title: 'Color',
+                    description: 'Starlight Aluminium Case w Starlight Sport Band'
+                },
+                {
+                    title: 'Style',
+                    description: 'GPS+41mm S/M - fits 130–180mm wrists'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '41 Millimeters'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71jOw9iquHL._AC_SX522_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71jOw9iquHL._AC_SX522_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71wgUDQfX9L._AC_SX522_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71wgUDQfX9L._AC_SX522_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/91d3XLmXUvL._AC_SX522_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/91d3XLmXUvL._AC_SX522_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71jOw9iquHL._AC_SX522_.jpg',
             name: 'Apple Watch Series 8 [GPS + Cellular 41mm] Smart Watch w/Starlight Aluminum Case with Starlight Sport Band - S/M. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On Retina Display, Water Resistant'
         },
         // https://www.amazon.com/Apple-Watch-Midnight-Aluminum-Always/dp/B0BDHM49BJ?ref_=ast_sto_dp
         {
+            id: `be89d174-36c9-4e67-a684-ce2b31624b97`,
+            price: '498.75',
+            brand: 'APPLE',
+            path: 'smartwatch',
+            information: [
+                {
+                    title: 'Shape',
+                    description: 'Square'
+                },
+                {
+                    title: 'Color',
+                    description: 'Midnight Aluminium Case w Midnight Sport Band'
+                },
+                {
+                    title: 'Style',
+                    description: '41mm M/L - fits 150–200mm wrists'
+                },
+                {
+                    title: 'Screen Size',
+                    description: '41 Millimeters'
+                },
+            ],
+            gallery: [
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71ulah9iIwL._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71ulah9iIwL._AC_SX679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61nZpsQlr5L._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61nZpsQlr5L._AC_SX679_.jpg',
+                },
+                {
+                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71MTaeb2J6L._AC_SX679_.jpg',
+                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71MTaeb2J6L._AC_SX679_.jpg',
+                }
+            ],
             img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71ulah9iIwL._AC_SX679_.jpg',
             name: 'Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On Retina Display, Water Resistant'
         },
